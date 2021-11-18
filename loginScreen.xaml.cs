15be49371425570118db16a7d1d8c187c3841372
nilsm5 <nils.m5@gso.schule.koeln>
2021-11-18 19:33:59 +0100

    Nils Loginscreen

---
+++ loginScreen.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace testwpf
+{
+    /// <summary>
+    /// Interaktionslogik für loginScreen.xaml
+    /// </summary>
+    public partial class loginScreen : Window
+    {
+        public loginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (txtUsername.Text != "" && txtPassword.Password != "")
+            {
+                MainWindow home = new MainWindow();
+                home.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Bitte geben Sie einen Benutzernamen und ein Passwort ein.");
+            }
+
+
+        }
+    }
+}
